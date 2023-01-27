09088bb0921335692a18cc12bdd7187ca7d5b479
SerhyRymar <serhyrymar@gmail.com>
2023-01-27 17:08:32 +0200

    Update_html

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,7 @@ import data from '../data/data.json';
 export const App = () => {
   return (
     <>
+      <h2>User profile</h2>
       <Profile
         username={user.username}
         tag={user.tag}
@@ -14,7 +15,10 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <h2>Statistics</h2>
       <Statistics title="Upload stats" stats={data} />
+      <h2>Friends</h2>
+      <h2>Transactions</h2>
     </>
   );
 };
