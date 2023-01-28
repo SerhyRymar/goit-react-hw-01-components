28163742ee1844717324d0c3790cbb6481c6e3f4
SerhyRymar <serhyrymar@gmail.com>
2023-01-28 21:05:45 +0200

    add_html_transaction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statictics/Statictics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 
 export const App = () => {
@@ -22,6 +24,7 @@ export const App = () => {
       <h2>Friends</h2>
       <FriendList friends={friends} />
       <h2>Transactions</h2>
+      <TransactionHistory items={transactions} />
     </>
   );
 };
