24347c0750b1b307cc559b47c61c8b9703f00bde
SerhyRymar <serhyrymar@gmail.com>
2023-01-29 14:05:35 +0200

    add_css_table

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,27 +1,28 @@
 import PropTypes from 'prop-types';
+import styles from './TransactionHistory.module.css';
 
 export const TransactionHistory = ({ items }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map(({ id, type, amount, currency }) => {
-          return (
-            <tr key={id}>
-              <td>{type}</td>
-              <td>{amount}</td>
-              <td>{currency}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+      <table className={styles.table}>
+        <thead className={styles.thead}>
+          <tr>
+            <th className={styles.table_th}>Type</th>
+            <th className={styles.table_th}>Amount</th>
+            <th className={styles.table_th}>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(({ id, type, amount, currency }) => {
+            return (
+              <tr key={id}>
+                <td className={styles.table_td}>{type}</td>
+                <td className={styles.table_td}>{amount}</td>
+                <td className={styles.table_td}>{currency}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
   );
 };
 
